070303d5bd447df8f887c35284b812e72b3955c3
Mahmudjon <mrfx404@gmail.com>
2026-02-18 17:09:50 +0500

    Yangilanish

--- Bitiruv/Bitiruv/urls.py
+++ Bitiruv/Bitiruv/urls.py
@@ -18,6 +18,8 @@ from django.contrib import admin
 from django.urls import path
 from django.contrib.auth import views as auth_views
 from .views import home
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -25,4 +27,6 @@ urlpatterns = [
     path('', home, name='home'),  # Bosh sahifa
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] 
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
--- Bitiruv/Bitiruv/views.py
+++ Bitiruv/Bitiruv/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from accounts.models import Profile
+from apps.accounts.models import Profile
 
 def home(request):
     profiles = Profile.objects.all()
